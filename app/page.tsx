1fe497aa94806994019678ee4502c7297711e766
datth216 <datwin0147@gmail.com>
2023-12-11 22:15:00 +0700

    add: header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Dropbox</h1>
+      <div className="pt-14 text-center bg-[#1e1919] pb-[72px] text-white dark:bg-[#020817]">
+        <div className="px-[250px]">
+          <div className="flex flex-col">
+            <h2 className="text-5xl font-sans">Securely collaborate on your content anywhere, anytime</h2>
+            <p className="mt-4 mb-8">With Dropbox, you get a full suite of tools designed to help you create, share, manage, and track content more efficiently. Plus, proven cloud storage you can trust.</p>
+            <Button className="bg-[#3984ff] max-w-[226px] p-6 mx-auto my-0 hover:bg-[#3985ffea]">Get started now</Button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ModeToggle } from './SelectMode/SelectMode'
+
+function Header() {
+    return (
+        <header className="flex items-center justify-between">
+            <Link href='/' className="flex">
+                <div className="bg-[#0061fe] w-fit flex items-center">
+                    <Image src="https://www.shareicon.net/data/64x64/2015/11/08/668675_box_512x512.png" className="invert px-2" width={50} height={50} alt='logo' />
+                </div>
+                <span className='py-4 px-2 text-2xl font-bold'>Dropbox</span>
+            </Link>
+            <div className="flex space-x-5 px-5">
+                <UserButton afterSignOutUrl='/' />
+                <SignedOut>
+                    <SignInButton afterSignInUrl='/' mode='modal'></SignInButton>
+                </SignedOut>
+                <ModeToggle />
+            </div>
+        </header>
+    )
+}
+
+export default Header
